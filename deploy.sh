7dca43fd6b836fc8a2b942e9b1e1644b76db249c
Kenny Jackelen <kennyjackelen@gmail.com>
2017-04-07 22:35:03 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+imageName=kenny/twinsgrid
+containerName=twinsgrid
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p 49162:8080 --name $containerName $imageName
